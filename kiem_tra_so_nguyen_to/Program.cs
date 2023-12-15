b2aff7a25c96cf81b1fbf3a466753d3ed7de3727
Hai2lai <152401246+Hai2lai@users.noreply.github.com>
2023-12-15 10:23:11 +0700

    kiem_tra_so_nguyen_to

---
+++ kiem_tra_so_nguyen_to/Program.cs
@@ -0,0 +1,32 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        float n;
+        Console.WriteLine("nhap 1 so ban muon kiem tra");
+        n = float.Parse(Console.ReadLine());
+        if(n<2)
+        {
+            Console.WriteLine(n+"khong phai la so nguyen to");
+        }else 
+        {   
+            bool kiemtra = true;
+            int i=2;
+            while(i<=Math.Sqrt(n))
+            {
+                if(n%i==0){
+                    kiemtra = false;
+                    break;
+                }
+                i++;
+            }
+            if(kiemtra)
+            {
+                Console.WriteLine("day la so nguyen to");
+            }else
+            {
+                Console.WriteLine("day khong phai la so nguyen to");
+            }
+        }
+    }
+}
